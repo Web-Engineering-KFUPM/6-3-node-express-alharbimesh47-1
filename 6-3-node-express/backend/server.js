c37a4fcb384b1d50f35e01aceac5808748846915
m3sh4al <aharbi419@gmail.com>
2026-04-06 10:01:31 +0300

    todo6

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -1,3 +1,5 @@
+import { getRandomQuote } from "./quotes.js";
+
 // TODO 1: Import and Create express app instance
 import express from "express";
 
@@ -17,9 +19,15 @@ import morgan from "morgan";
 app.use(morgan("dev"));
 
 // TODO 6.1: Create root route "/"
-
+app.get("/", (req, res) => {
+    res.send("Welcome to the Quote API");
+  });
 
 // TODO 6.2: Create "/api/quote" route
 
+app.get("/api/quote", (req, res) => {
+    const quote = getRandomQuote();
+    res.json({ quote });
+  });
 
 // TODO 7: Start server using app.listen
